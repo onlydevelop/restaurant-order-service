b972410eed9623212e3bc610bcd1bc2c8360e21d
Dipanjan Bhowmik <onlydevelop@gmail.com>
2018-07-04 22:32:54 +0530

    Added zuul.

--- src/main/java/com/restaurant/orderservice/OrderController.java
+++ src/main/java/com/restaurant/orderservice/OrderController.java
@@ -1,5 +1,7 @@
 package com.restaurant.orderservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private ItemServiceProxy itemServiceProxy;
 	
 	@PostMapping("/orders/type/{type}")
 	public Order getOrder(@PathVariable String type, @RequestBody LineItem[] lineItems) {
+		logger.info("order-service: type -> {}", type);
 		Order order = new Order();
 		for(LineItem lineItem : lineItems) {
 			
