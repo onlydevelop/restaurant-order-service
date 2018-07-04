4dbb10214950d967cb7d5024375140fcce9c18ca
Dipanjan Bhowmik <onlydevelop@gmail.com>
2018-07-04 15:43:59 +0530

    Route call through API Gateway server

--- src/main/java/com/restaurant/orderservice/ItemServiceProxy.java
+++ src/main/java/com/restaurant/orderservice/ItemServiceProxy.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="item-service")
+@FeignClient(name="api-gateway-server")
 @RibbonClient(name="item-service")
 public interface ItemServiceProxy {
-	@GetMapping("/items/{id}/type/{type}")
+	@GetMapping("/item-service/items/{id}/type/{type}")
 	public Item getItem(@PathVariable("id") Long id, @PathVariable("type") String type);
 }
