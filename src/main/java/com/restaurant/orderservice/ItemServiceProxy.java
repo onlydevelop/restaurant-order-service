aedef6d14a92a6e1491ebbd03dbeb29c950e9d0e
Dipanjan Bhowmik <onlydevelop@gmail.com>
2018-07-04 13:03:56 +0530

    Added Ribbon for load balancing.

--- src/main/java/com/restaurant/orderservice/ItemServiceProxy.java
+++ src/main/java/com/restaurant/orderservice/ItemServiceProxy.java
@@ -1,10 +1,12 @@
 package com.restaurant.orderservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="item-service", url="http://localhost:10000")
+@FeignClient(name="item-service")
+@RibbonClient(name="item-service")
 public interface ItemServiceProxy {
 	@GetMapping("/items/{id}/type/{type}")
 	public Item getItem(@PathVariable("id") Long id, @PathVariable("type") String type);
